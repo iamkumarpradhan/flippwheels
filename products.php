d1ae596b85d5c0ecfaf35840a553417496d370e0
iamkumarpradhan <78998332+iamkumarpradhan@users.noreply.github.com>
2022-04-25 13:58:34 +0545

    uploads

--- products.php
+++ products.php
@@ -14,17 +14,13 @@ catch(PDOException $e){
 
 <div class="flex">
     <?php foreach($products as $product): ?>
-    <div class="item">
-        <img src="<?php echo "http://localhost/flippwheels/uploads/".$product['photo'] ;?>" alt="" width="300px" height="300px">
-        <h2><?php echo $product['name'];?></h2>
-        <h4 class="txt">Rs.<?=$product['price'];?></h4>
-        <h4><?php echo $product['fuel'];?></h4>
-
-        <a href="details.php?id=<?php echo $product['id'];?>">
-            <button type="button" class="btn btn-success" data-toggle="modal" data-targets="#details-1" >Know More</button>
-        </a>
-        <button type="submit" class="btn btn-info">Add to Cart</button>
+    <div class='item'>
+        <img src="<?php echo "http://localhost/flipwheels/uploads/".$product['photo'] ;?>" alt="" width="200px" height="200px">
+        <h4><?php echo $product['name'];?></h4>
+        <h5><?php echo $product['price'];?></h4>
     </div>
     <?php endforeach;?>
 </div>
+
+
 <?php include_once('./template/footer.php'); ?> 
