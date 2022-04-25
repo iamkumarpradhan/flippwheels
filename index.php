1a193fc8335b36b7e30e1d980b1cc6dbdc20d001
iamkumarpradhan <78998332+iamkumarpradhan@users.noreply.github.com>
2022-04-25 13:42:06 +0545

    index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php 
+include_once('./template/header.php');
+?>
+<div class="center">
+    <!-- <h1>ARE YOUR READY TO FLIPP YOUR OLD AUTOWHEELS?</h1> -->
+   
+    <div class="photos">
+        <img src="./uploads/marchie.jpg">
+        <h3>SELL YOUR WHEELS AT BEST PRICE!</h3>
+    </div>
+
+    <div class="image">
+        <h3>All in 3 Simple Steps</h3>
+        <img src="./uploads/allin3.png">
+    </div>
+    <div class="image">
+        <h3>Why should you choose FLIPPWHEELS?</h3>
+        <img src="./uploads/choose.png">
+    </div>
+</div>
+<?php include_once("./template/footer.php"); ?> 
